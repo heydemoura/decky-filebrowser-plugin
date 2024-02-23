76ce47845e0e92e95b17451b0e7377cdc73f3669
Heyde Moura <moura.heyde@gmail.com>
2024-02-23 09:28:50 -0300

    Add script to sync build to my deck"

---
+++ scripts/sync.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+FILES_LIST="dist bin LICENSE main.py package.json plugin.json README.md"
+DECK_IP="192.168.0.125"
+HOMEBREW_DIR="/home/deck/homebrew"
+
+PLUGIN_DIR="plugins/decky-filebrowser"
+# Create folder at destination
+ssh deck@$DECK_IP "mkdir $HOMEBREW_DIR/$PLUGIN_DIR"
+
+
+echo "Copying files to Steam Deck:"
+for i in $FILES_LIST; do
+    scp -r $i deck@$DECK_IP:$HOMEBREW_DIR/plugins/decky-filebrowser
+done

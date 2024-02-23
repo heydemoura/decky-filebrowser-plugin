186946b53229aff2b61d58f1d207e3bdcaf7d099
Heyde Moura <moura.heyde@gmail.com>
2024-02-23 11:23:55 -0300

    Sync: add cleanup before syncing

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -8,6 +8,11 @@ PLUGIN_DIR="plugins/decky-filebrowser"
 # Create folder at destination
 ssh deck@$DECK_IP "mkdir $HOMEBREW_DIR/$PLUGIN_DIR"
 
+ssh deck@$DECK_IP "chmod a+w -R $HOMEBREW_DIR/$PLUGIN_DIR"
+
+# Cleaning directory
+echo "Cleaning old files"
+ssh deck@$DECK_IP "rm -rf $HOMEBREW_DIR/$PLUGIN_DIR/*"
 
 echo "Copying files to Steam Deck:"
 for i in $FILES_LIST; do

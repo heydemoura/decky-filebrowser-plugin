ac5e5d8ddaeb0e27fae8f94593ae4cc942119d8a
Heyde Moura <moura.heyde@gmail.com>
2024-03-19 20:31:17 -0300

    Add File Browser build on CI

---
+++ backend/entrypoint.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env sh
+
+#!/bin/sh
+set -e
+
+echo "Container's IP address: `awk 'END{print $1}' /etc/hosts`"
+
+cd /backend
+
+mkdir -p out
+
+echo "--- Building File Browser front end ---"
+git clone https://github.com/filebrowser/filebrowser filebrowser
+chmod a+w -R filebrowser
+cd filebrowser/frontend
+npm install
+npm run build
+
+echo "--- Building File Browser back end ---"
+cd ../
+go mod download
+go build -o filebrowser
+cp filebrowser /backend/out
+
+echo "--- Cleanup File Browser repo ---"
+cd /backend
+rm -rf filebrowser

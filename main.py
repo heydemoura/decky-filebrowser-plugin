ae5e7413b9ee0d046f3db8ae52f787fa3f36aa17
Heyde Moura <moura.heyde@gmail.com>
2024-03-20 08:12:50 -0300

    Adding database path option to start command

--- main.py
+++ main.py
@@ -16,6 +16,10 @@ settingsDir = decky_plugin.DECKY_PLUGIN_SETTINGS_DIR
 script_dir = decky_plugin.DECKY_PLUGIN_DIR
 pidfile = decky_plugin.DECKY_PLUGIN_RUNTIME_DIR + "/decky-filebrowser.pid"
 
+# Strings useful for starting File Browser
+filebrowser_bin = decky_plugin.DECKY_PLUGIN_DIR + "/bin/filebrowser"
+filebrowser_database_path = decky_plugin.DECKY_PLUGIN_SETTINGS_DIR + "/filebrowser.db"
+
 # Load user's settings
 settings = SettingsManager(name="settings", settings_directory=settingsDir)
 settings.read()
@@ -41,7 +45,9 @@ class Plugin:
             }
 
     async def startFileBrowser( self, port = 8082 ):
-        command = script_dir + "/bin/filebrowser -p " + str(port) + " -a 0.0.0.0 -r " + decky_plugin.DECKY_USER_HOME
+        command = filebrowser_bin + " -p " + str(port) + " -a 0.0.0.0" + " -d " + filebrowser_database_path + " -r " + decky_plugin.DECKY_USER_HOME
+
+        decky_plugin.logger.info("Running FileBrowser command: " + command)
         process = await asyncio.create_subprocess_shell(command, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
 
         # Defines the received port as the port setting
